b51daa7e13991deae793b35aa7e3720f50707184
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-02-14 18:52:37 +0200

    Update Palgad.py

--- Palgad/Palgad.py
+++ Palgad/Palgad.py
@@ -1,14 +1,13 @@
 from OmaMoodul import *
 
-
-palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
+palgad=[1200.2333, 2500,750,395,1200]
+inimesed=["A","B","C","E","D"]
 
 while True:
     print(inimesed)
     print(palgad)
     print()
-    menu=int(input("valik:\n1-lisa andmed\n2-Kustuta andmed\n3-Suurim palk\n4-Vähem palk\n5-Sort\n6-viige inimesi välja täiusliku salgataga\n7-otsi palka inimese nime järgi\n8-sisestatud palga põhjal kuvab inimeste madalamad ja kõrgemad palgad\n9-Leia top 3 vähem või rohkem\n10-Otsi keskmine palk ja kes on saada suurem\n11-Otsi maksuvaba palk\n12-Sort\n13-Otsige kes teenivad alla keskmise palga ja emalda nad\n14-Teeb nimekiri\n15-saate teada 5% tõusuga töötaja töötasu T-aastas\n"))
+    menu=int(input("valik:\n1-lisa andmed\n2-Kustuta andmed\n3-Suurim palk\n4-Vähem palk\n5-Sort\n6-viige inimesi välja täiusliku salgataga\n7-otsi palka inimese nime järgi\n8-sisestatud palga põhjal kuvab inimeste madalamad ja kõrgemad palgad\n9-Leia top 3 vähem või rohkem\n10-Otsi keskmine palk ja kes on saada suurem\n11-Otsi maksuvaba palk\n12-Sort\n13-Otsige kes teenivad alla keskmise palga ja emalda nad\n14-Teeb nimekiri ilusam ja mugavam\n15-saate teada 5% tõusuga töötaja töötasu T-aastas\n16-Nimetage ümber iga 3 inimese järel \n"))
     if menu==0:
         break
     elif menu==1:
@@ -44,10 +43,10 @@ while True:
     elif menu==13:
         inimesed,palgad=emalda(inimesed,palgad)
     elif menu==14:
-        inimesed,palgad=tint(inimesed,palgad)
+        inimesed,palgad=kiri(inimesed,palgad)
     elif menu==15:
         inimesed,palgad=year(inimesed,palgad)
     elif menu==16:
-        renamed_inimesed = rename(inimesed, new)
+        inimesed=rename(inimesed)
     else:
         print("Kirjuta õige arv")
