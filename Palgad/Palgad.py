7dc7e361770146cde17e9bfc7ce1c6fe6d5283c4
MaksimTse <120181800+MaksimTse@users.noreply.github.com>
2023-02-14 19:14:39 +0200

    Update Palgad.py

--- Palgad/Palgad.py
+++ Palgad/Palgad.py
@@ -4,49 +4,93 @@ palgad=[1200.2333, 2500,750,395,1200]
 inimesed=["A","B","C","E","D"]
 
 while True:
-    print(inimesed)
-    print(palgad)
+
     print()
     menu=int(input("valik:\n1-lisa andmed\n2-Kustuta andmed\n3-Suurim palk\n4-Vähem palk\n5-Sort\n6-viige inimesi välja täiusliku salgataga\n7-otsi palka inimese nime järgi\n8-sisestatud palga põhjal kuvab inimeste madalamad ja kõrgemad palgad\n9-Leia top 3 vähem või rohkem\n10-Otsi keskmine palk ja kes on saada suurem\n11-Otsi maksuvaba palk\n12-Sort\n13-Otsige kes teenivad alla keskmise palga ja emalda nad\n14-Teeb nimekiri ilusam ja mugavam\n15-saate teada 5% tõusuga töötaja töötasu T-aastas\n16-Nimetage ümber iga 3 inimese järel \n"))
     if menu==0:
         break
     elif menu==1:
         inimesed, palgad=Lisa_andmed(inimesed, palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==2:
         imimesed,palgad=Kustutamine(inimesed, palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==3:
         palk,nimi=Suurim_palk(inimesed,palgad)
         print("Suurim palk on",palk, nimi)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==4:
         palk,nimi=Vähem_palk(inimesed,palgad)
         print("vähem palk pn", palk, nimi)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==5:
         inimesed,palgad=Soorteerimine(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==6:
         iniemsed,palgad=Vordsed_palgad(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==7:
         iniemsed,palgad=imja(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==8:
         amount = int(input("Sisestage summa, millega võrrelda: "))
         more_than = disp(palgad, inimesed, amount, "suurim")
         less_than = disp(palgad, inimesed, amount, "vähem")
         print(f"Inimesed, kellel on palk rohkem kui {amount}:", more_than)
         print(f"Inimesed, kelle palk on väiksem kui {amount}:", less_than)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==9:
         top(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==10:
         kesk(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==11:
         tulu(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==12:
         inimesed,palgad=sort(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==13:
         inimesed,palgad=emalda(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==14:
         inimesed,palgad=kiri(inimesed,palgad)
+        print()
+        print(inimesed,palgad)
+        print()
     elif menu==15:
         inimesed,palgad=year(inimesed,palgad)
     elif menu==16:
         inimesed=rename(inimesed)
+        print()
+        print(inimesed,palgad)
+        print()
     else:
         print("Kirjuta õige arv")
